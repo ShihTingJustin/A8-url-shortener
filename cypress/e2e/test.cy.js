6c3a8e1fc287491f9f0db80f277380e80477da4f
ShihTingJustin <justinhuang777@gmail.com>
2023-04-10 22:10:57 +0800

    test: add e2e test case

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -5,17 +5,50 @@
 若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者
 */
 
-describe("template spec", () => {
-  it("passes", () => {
+describe("測試 spec", () => {
+  beforeEach(() => {
     cy.viewport(1280, 800);
     cy.visit("");
+  });
+
+  it("若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者", () => {
+    cy.get('[data-testid="url-input"]').should("be.visible");
+    cy.get('[data-testid="url-input"]').should("have.prop", "required", true);
+  });
+
+  it("輸入相同網址時，產生一樣的縮址", () => {
+    let shortLink = "";
 
     cy.get('[data-testid="url-input"]').should("be.visible");
     cy.get('[data-testid="url-input"]').click();
     cy.get('[data-testid="url-input"]').type("https://www.sthdev.app/");
-
     cy.get('[data-testid="submit-btn"]').click();
+    cy.get('[data-testid="short-url"]')
+      .invoke("text")
+      .then((shortUrl) => {
+        shortLink = shortUrl;
+        cy.request({ method: "GET", url: shortUrl }).then((res) => {
+          expect(res.status).to.equal(200);
+        });
+      });
 
+    cy.visit("");
+
+    cy.get('[data-testid="url-input"]').click();
+    cy.get('[data-testid="url-input"]').type("https://www.sthdev.app/");
+    cy.get('[data-testid="submit-btn"]').click();
+    cy.get('[data-testid="short-url"]')
+      .invoke("text")
+      .then((shortUrl) => {
+        expect(shortUrl).to.equal(shortLink);
+      });
+  });
+
+  it("產生的短網址可以正常運作", () => {
+    cy.get('[data-testid="url-input"]').should("be.visible");
+    cy.get('[data-testid="url-input"]').click();
+    cy.get('[data-testid="url-input"]').type("https://www.sthdev.app/");
+    cy.get('[data-testid="submit-btn"]').click();
     cy.get('[data-testid="short-url"]')
       .invoke("text")
       .then((shortUrl) => {
